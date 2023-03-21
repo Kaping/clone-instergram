e84002b8b2b67ba88e88591bc1723ff896fd71e0
수형 염 <ellisa20106@inu.ac.kr>
2023-03-21 20:10:22 +0900

    new widget and image

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,44 @@
 import 'package:flutter/material.dart';
+import './style.dart' as style;
 
 void main() {
   runApp(
       MaterialApp(
-          theme: ThemeData(
-
-            iconTheme: IconThemeData(color: Colors.white),
-
-            appBarTheme: AppBarTheme(
-                elevation: 1,
-                color: Colors.white,
-                titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
-                actionsIconTheme: IconThemeData(color: Colors.black, size: 40)
-            ),
-
-            textTheme: TextTheme(
-                bodyText2: TextStyle(color: Colors.red)
-            )
-          ),
+          theme: style.theme,
           home: MyApp()
       )
   );
 }
 var a = TextStyle(color: Colors.black, fontSize: 25);
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class home extends StatelessWidget {
+  const home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+  return Column(
+      children: [
+        Image(image: AssetImage("images/example.png")),
+        Text("난데요"),
+        Text("좋아요가 많아요")
+      ],
+    );
+  }
+}
+class _MyAppState extends State<MyApp> {
+
+  var tab = 0;
+
+
+
+  @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -38,11 +49,28 @@ class MyApp extends StatelessWidget {
               icon: Icon(Icons.add_box_outlined),)
         ],
       ),
-      body: Text('하이', style: a,),
-      // Icon(Icons.star, color: Colors.blue),
-
-
+      body: Column(
+        children: [
+          Image.asset('assets/example.png'),
+        ],
+      ),
+      // ,[Text("집"), Text("마트")][tab],
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: (i){
+          setState(() {
+            tab = i;
+          });
+        },
+        items: [
+           BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
+           BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label: '샵'),
+        ],
+      ),
     );
+
   }
 }
+
 
---
+++ lib/style.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+var _var1; //if use under bar,like '_varName', then you can't use this var other files. 언더바 쓰면 다른 파일에서 못쓰게 됨
+
+var theme = ThemeData(
+
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        backgroundColor: Colors.grey,
+
+      )
+    ),
+
+    iconTheme: IconThemeData(color: Colors.black),
+
+    appBarTheme: AppBarTheme(
+      elevation: 1,
+      color: Colors.white,
+      titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
+      actionsIconTheme: IconThemeData(color: Colors.black, size: 40)
+    ),
+
+
+    textTheme: TextTheme(
+      bodyText2: TextStyle(color: Colors.red)
+    ),
+
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: Colors.black,
+
+    )
+);
